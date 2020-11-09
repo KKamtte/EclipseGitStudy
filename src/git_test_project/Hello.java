3f074fb3665688ec35a8d1f67c0c132169c916f3
KKAMTTE <swye199@gmail.com>
2020-11-09 13:48:32 +0900

    최초 커밋

---
+++ src/git_test_project/Hello.java
@@ -0,0 +1,7 @@
+package git_test_project;
+
+public class Hello {
+	public void say() {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/git_test_project/Main.java
@@ -0,0 +1,10 @@
+package git_test_project;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Hello().say();
+	}
+
+}
